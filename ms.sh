c36a67931fe587a4575c5d7015cfa7ac6dec1800
MinsukJi-NOAA <minsuk.ji@noaa.gov>
2021-04-05 11:22:38 -0400

    test

--- ms.sh
+++ ms.sh
@@ -3,3 +3,5 @@ echo "== HELLO =="
 echo $GITHUB_WORKSPACE
 echo "==========="
 cat $GITHUB_EVENT_PATH
+echo $GITHUB_TOKEN
+#curl -sS -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}"  -H "Accept: application/vnd.github.v3+json"
--- pt.py
+++ pt.py
@@ -3,4 +3,8 @@
 import json
 import os
 import sys
-from github import Github
+from github import Github as g
+
+#g = Github(${{ secrets.GITHUB_TOKEN }})
+branch = g.get_repo("ufs-community/ufs-weather-model").get_branch("master")
+print(branch.commit)
