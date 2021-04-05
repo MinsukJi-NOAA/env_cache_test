e0f3ff742809e1fa6c4b996ebba2f1c7becc9c68
MinsukJi-NOAA <minsuk.ji@noaa.gov>
2021-04-05 11:48:31 -0400

    test env

--- ms.sh
+++ ms.sh
@@ -3,5 +3,10 @@ echo "== HELLO =="
 echo $GITHUB_WORKSPACE
 echo "==========="
 cat $GITHUB_EVENT_PATH
-echo $GITHUB_TOKEN
+echo "==========="
+echo $LETS_SEE
+echo "==========="
+LETS_SEE=no
+echo $LETS_SEE
+echo "==========="
 #curl -sS -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}"  -H "Accept: application/vnd.github.v3+json"
